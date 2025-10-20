74546eea1994c1cf7eef275b2d74a1c85cc073c5
Zhou Shi <zhoushi.vip@gmail.com>
2025-10-20 09:38:11 +0700

    inisialisai awal

---
+++ resources/views/layouts/adminlte.blade.php
@@ -0,0 +1,19 @@
+@extends('adminlte::page')
+
+@section('title', 'AdminLTE')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
+
+@section('content')
+    <p>Welcome to this beautiful admin panel.</p>
+@stop
+
+@section('css')
+    {{-- <link rel="stylesheet" href=""> --}}
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\KurikulumConttroller;
+use App\Http\Controllers\ProfileController;
+use App\Models\Kurikulum;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route::get('/dashboard', function () {
+//     return view('adminlte');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('kurikulum', KurikulumConttroller::class);
+});
+
+require __DIR__.'/auth.php';
